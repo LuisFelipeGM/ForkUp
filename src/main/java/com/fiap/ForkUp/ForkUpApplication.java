5ea4bb57ccf047782b4f27892e53c9ea69120970
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2026-04-13 23:03:56 -0300

    initial commit

---
+++ src/main/java/com/fiap/ForkUp/ForkUpApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ForkUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ForkUp/ForkUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ForkUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
